995e5d593807bee5f21630dafbc659f3cbe1903f
kdh <kds6723@naver.com>
2024-06-10 18:25:56 +0900

    06-10

--- spring1/src/main/java/com/itwill/spring1/web/ExampleController.java
+++ spring1/src/main/java/com/itwill/spring1/web/ExampleController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.itwill.spring1.dto.UserDto;
 
@@ -103,5 +104,27 @@ public class ExampleController {
     	// 리다이렉트 방식의 페이지 이동 - 요청 주소가 리다이렉트 되는 주소로 바뀜!
     }
     
+    @GetMapping("/rest1")
+    @ResponseBody
+    //-> 컨트롤러 메서드가 리턴하는 값이 뷰를 찾기 위한 문자열이 아니라,
+    // 클라이언트로 직접 응답되는 데이터. 
+    // 응답 패킷(response packet)의 몸통(body)에 포함되는 데이터.
+    public String rest1() {
+    	log.debug("rest1()");
+    	
+    	
+    	return "Hello. 안녕하세요!";
+    }
+    
+    @GetMapping("/rest2")
+    @ResponseBody //-> 리턴 값이 클라이언트로 직접 응답되는 객체.
+    public UserDto rest2() {
+    	log.debug("rest2()");
+    	
+    	return UserDto.builder().username("홍길동").age(99).build();
+    	//-> REST 컨트롤러가 리턴한 자바 객체를 jackson-databind 라이브러리에
+    	// JSON(JavaScript Object Notation) 형식의 문자열로 변환하고,
+    	// 클라이언트로 응답을 보냄.
+    }
     
 }
