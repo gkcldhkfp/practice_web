950e3a86e1b794e422c207826c6bb17f1625aebd
kdh <kds6723@naver.com>
2024-07-25 18:19:56 +0900

    07-25

--- springboot3/src/main/java/com/itwill/springboot3/service/DepartmentService.java
+++ springboot3/src/main/java/com/itwill/springboot3/service/DepartmentService.java
@@ -3,7 +3,12 @@ package com.itwill.springboot3.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.itwill.springboot3.domain.Department;
 import com.itwill.springboot3.repository.DepartmentRepository;
@@ -19,9 +24,12 @@ public class DepartmentService {
 	@Autowired
 	private final DepartmentRepository deptRepo;
 	
-	public List<Department> read() {
+	@Transactional(readOnly = true)
+	public Page<Department> read(int pageNo, Sort sort) {
+		log.info("read()");
+		Pageable pageable = PageRequest.of(pageNo, 5, sort);
 		
-		return deptRepo.findAll();
+		return deptRepo.findAll(pageable);
 	}
 	
 	public Department read(Integer id) {
--- springboot3/src/main/java/com/itwill/springboot3/web/DepartmentController.java
+++ springboot3/src/main/java/com/itwill/springboot3/web/DepartmentController.java
@@ -1,11 +1,17 @@
 package com.itwill.springboot3.web;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.itwill.springboot3.domain.Department;
 import com.itwill.springboot3.service.DepartmentService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,11 +26,13 @@ public class DepartmentController {
 	private final DepartmentService deptSvc;
 	
 	@GetMapping("/list")
-	public String list(Model model) {
+	public void list(@RequestParam(name="p", defaultValue = "0") int pageNo, Model model) {
+		log.info("list()");
 		
-		model.addAttribute("departments", deptSvc.read());
+		Page<Department> list = deptSvc.read(pageNo, Sort.by("id"));
 		
-		return "department/list";
+		model.addAttribute("page", list);
+		
 	}
 	
 	@GetMapping("/details/{id}")
